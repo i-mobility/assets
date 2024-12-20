54957eb7cce005fef9c61d983f72054c8ab71537
simonbility <simon.anreiter@wegfinder.at>
2024-12-20 12:21:11 +0100

    Cleanup

--- verify.py
+++ verify.py
@@ -110,7 +110,7 @@ all_icons = set()
 
 def get_image_size(path):
     return check_output([
-    'identify', '-format', '%wx%h', path
+    'magick', 'identify', '-format', '%wx%h', path
     ]).decode('utf-8')
     
 
