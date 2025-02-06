0f965b822b7d1e374bc69a7cfee23fe980825335
simonbility <31699786+simonbility@users.noreply.github.com>
2025-02-06 10:36:17 +0100

    Verify  translations (#38)
    Co-authored-by: simonbility <simon.anreiter@wegfinder.at>

--- verify.py
+++ verify.py
@@ -11,6 +11,7 @@ from PIL import Image
 CORRECT = os.getenv('CORRECT_ASSETS', 'False').lower() in ('true', '1', 't')
 COLOR_PATTERN = re.compile(r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
 RESOLUTIONS = ('mdpi', 'hdpi', 'xhdpi', 'xxhdpi')
+LANGUAGES = ('en', 'de')
 TRANSPORT_ICON_TYPES = ('icon', 'secondary_icon', 'group_icon', 'indicator')
 EXPECTED_SIZES = {
     'transport.icon': {
@@ -46,37 +47,58 @@ EXPECTED_SIZES = {
 }
 
 transport_icons = defaultdict(set)
-issues = list()
+errors = list()
 
 
 if CORRECT:
     shutil.copytree('images', 'images_corrected')
 
 
-def expect(condition, message, path=None):
-    if not condition:
-        if not path:
-            print(f'::warning:: {message}')
-        else:
-            print(f'::warning file={path}:: {message}')
+def log(message, severity = 'warning', path=None):
+    if not path:
+        print(f'::{severity}:: {message}')
+    else:
+        print(f'::{severity} file={path}:: {message}')
 
-        issues.append(message)
+    if severity == 'error':
+        errors.append(message)
+
+def expect(condition, message, severity = 'warning', path=None):
+    if not condition:
+        log(message, severity = severity, path = path)
     return condition
+
 
 with open('definitions.json') as fh:
     defs = load(fh)
 
+translations = dict()
+
+for language in LANGUAGES:
+    with open(f'translations/{language}.json') as fh:
+        translations[language] = load(fh)
+
+def check_translation_key(key):
+    for language in LANGUAGES:
+        if not translations[language].get(key):
+            log(f'translation {key} missing for {language}', severity = 'error')
+
 print('Checking definitions.json…')
 print('Checking transport…')
 for mapping in defs['transport']:
-    if not expect('id' in mapping, '"id" is required for each transport mapping'):
+    if not expect('id' in mapping, severity = 'error', message = '"id" is required for each transport mapping'):
         continue
 
     print(f'- {mapping["id"]}')
 
+    if 'translation_key' not in mapping:
+        mapping['translation_key'] = f'transportation_label.{mapping["id"]}'
+  
+    check_translation_key(mapping['translation_key'])
+    
     for icon_type in TRANSPORT_ICON_TYPES:
         icon = mapping.get(icon_type, dict())
-      
+
         if isinstance(icon, str):
             icon = { 'default': icon }
 
@@ -91,20 +113,35 @@ for mapping in defs['transport']:
                 transport_icons['transport.' + icon_type].add(value + '.png')
 
     if 'color' in mapping:
-        expect(COLOR_PATTERN.search(mapping['color']), f'{mapping["id"]} has invalid color {mapping["color"]}')
+        expect(COLOR_PATTERN.search(mapping['color']), severity = 'error', message = f'{mapping["id"]} has invalid color {mapping["color"]}')
 
-print('Checking redeem_code.sponsors…')
+print('Checking redeem_code.providers…')
 for mapping in defs['redeem_code']['providers']:
-    if not expect('id' in mapping, '"id" is required for each provider mapping'):
+    if not expect('id' in mapping, severity = 'error', message = '"id" is required for each provider mapping'):
         continue
-    if not expect('icon' in mapping, '"icon" is required for each provider mapping'):
+
+    print(f'- {mapping["id"]}')
+    
+    if not expect('icon' in mapping, severity = 'error', message = '"icon" is required for each provider mapping'):
         continue
-    print(f'Checking sponsor mapping {mapping["id"]}…')
+
+    if expect('translation_key' in mapping, severity = 'error', message = '"translation_key" is required for each provider mapping'):
+        check_translation_key(mapping['translation_key'])
 
     transport_icons['redeem_code.provider'].add(mapping['icon'] + '.png' )
 
-    if 'color' in mapping:
-        expect(COLOR_PATTERN.search(mapping['color']), f'{mapping["id"]} has invalid color {mapping["color"]}')
+    if expect('color' in mapping, severity = 'error', message = '"color" is required for each provider mapping'):
+        expect(COLOR_PATTERN.search(mapping['color']), severity = 'error', message = f'{mapping["id"]} has invalid color {mapping["color"]}')
+
+print('Checking redeem_code.sponsors')
+for mapping in defs['redeem_code']['sponsors']:
+    if not expect('id' in mapping, severity = 'error', message = '"id" is required for each provider mapping'):
+        continue
+
+    print(f'- {mapping["id"]}')
+    
+    if expect('translation_key' in mapping, severity = 'error', message = '"translation_key" is required for each provider mapping'):
+        check_translation_key(mapping['translation_key'])
 
 all_icons = set()
 
@@ -119,10 +156,11 @@ for type, icons in transport_icons.items():
 
     print(f'- {type}')
     for i in icons:
+        print(f'  - {i}')
         for res in RESOLUTIONS:
             path = Path('images') / Path(res) / Path(i)
 
-            if not expect(path.exists(), f'{i} in {res} does not exist.'):
+            if not expect(path.exists(), severity = 'error', message = f'{i} in {res} does not exist.'):
                 continue
 
             expected_size = EXPECTED_SIZES[type][res]
@@ -139,7 +177,7 @@ for type, icons in transport_icons.items():
                                 os.remove(destination_path)
                             shutil.copyfile(path, destination_path)
 
-            expect(actual_size == expected_size, f'Expected {path} to have size {expected_size} got {actual_size}{extra_info}', path = path)
+            expect(actual_size == expected_size, message = f'Expected {path} to have size {expected_size} got {actual_size}{extra_info}', path = path)
            
 
 print('Checking for stray icons…')
@@ -149,7 +187,5 @@ for res in RESOLUTIONS:
             print(f'::warning file={entry}::stray icon {entry}')  # entry.unlink()
 
 
-# if len(issues) == 0:
-#     print('All good 👍')
-# else:
-#     sys.exit(1)
+if len(errors) > 0:
+    sys.exit(1)
